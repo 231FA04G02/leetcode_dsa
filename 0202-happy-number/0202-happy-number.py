7f04ced7964aa08e856358d9db607169eb9577e4
Ayush singh <singhayush5432@gmail.com>
2026-01-26 23:13:30 +0530

    Time: 3 ms (40.81%), Space: 19.4 MB (22.59%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        dict={}
+        while n!=1:
+            if n in dict:
+                return False
+
+            dict[n]=1
+
+            s=0
+            for digit in str(n):
+                s+=int(digit)**2
+                n=s
+        return True
+
+
+            
